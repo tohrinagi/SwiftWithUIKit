1ae812e70de20c70540c7a780415fd0cf9233553
tohrinagi <tohrinagi@gmail.com>
2014-10-07 02:32:43 +0900

    Initial Commit

---
+++ SwiftWithUIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWithUIKit
+//
+//  Created by tohrinagi on 2014/10/07.
+//  Copyright (c) 2014年 tohrinagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
